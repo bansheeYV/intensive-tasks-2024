91b22ab7492ecf4c6c3bb21ab3a7721923c16a62
bansheeYV <bansheew2@gmail.com>
2024-12-15 22:58:06 +0300

    Task1 исправил замечания

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,25 +15,24 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 11;
+        int age = 1111;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        String result = "";
         if (age < 0){
-            return result = "Некорректный ввод";
+            return "Некорректный ввод";
         }
-        if (age == 0 || (age % 100 > 10 && age % 100 < 20) || (age > 10 && age < 20)) {
-            result = "Вам " + age + " лет";
-        } else if ((age == 1) || (age % 10 == 1 && age != 11)){
-                result = "Вам " + age + " год";
-        } else if ((age > 1 && age < 5) || (age % 10 > 1 && age % 10 < 5)){
-            result = "Вам " + age + " года";
-        } else if ((age > 4 && age < 10) || (age % 10 == 0 || age % 10 > 4 || age % 10 < 10)){
-          result = "Вам " + age + " лет";
+
+        if (age % 10 == 1 && age % 100 != 11) {
+            return "Вам " + age + " год";
         }
-        return result;// Заглушка. При реализации - удалить
+
+        if (age % 10 > 1 && age % 10 < 5 && !(age % 100 > 10 && age % 100 < 15)) {
+            return "Вам " + age + " года";
+        }
+
+        return "Вам " + age + " лет";
     }
 }
