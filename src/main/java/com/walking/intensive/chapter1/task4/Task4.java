c29024daadaabff191614204f6f48df7efbb5d25
bansheeYV <bansheew2@gmail.com>
2024-12-16 09:20:25 +0300

    Task4 исправил замечания

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -29,41 +29,38 @@ public class Task4 {
         double b = 0;
         double c = 0;
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        String result = "";
-        double discriminant = (b * b) - (4 * a * c);
+        double discriminant = b * b - 4 * a * c;
 
-        if (a == 0 && b == 0 && c == 00){
-            return result = "Бесконечное множество решений.";
+        if (a == 0 && b == 0 && c == 0){
+            return "Бесконечное множество решений.";
         }
 
-        if (discriminant < 0 || (a == 0 && b == 0)){
-            return result = "Количество решений: 0.";
+        if (discriminant < 0 || a == 0 && b == 0){
+            return "Количество решений: 0.";
         }
 
         if (discriminant == 0 && b == 0 && c == 0){
-            return result = "Количество решений: 1. Корень: 0.0";
+            return "Количество решений: 1. Корень: 0.0";
         }
 
         if (discriminant == 0){
-            return result = "Количество решений: 1. Корень: " + ((-b) / (2 * a));
+            return "Количество решений: 1. Корень: " + (-b / (2 * a));
         }
 
-        if (discriminant > 0 && a == 0){
-            return result = "Количество решений: 1. Корень: " + ((-c) / (b));
+        if (a == 0){
+            return "Количество решений: 1. Корень: " + (-c / b);
         }
 
-        if (discriminant > 0){
-            double x1 = ((-b - Math.sqrt(discriminant)) / (2 * a));
-            double x2 = ((-b + Math.sqrt(discriminant)) / (2 * a));
-            if (x1 < x2){
-                result = "Количество решений: 2. Корни: " + x1 + ";" + x2;
-            } else {
-                result = "Количество решений: 2. Корни: " + x2 + ";" + x1;
-            }
+        String result = "";
+        double x1 = ((-b - Math.sqrt(discriminant)) / (2 * a));
+        double x2 = ((-b + Math.sqrt(discriminant)) / (2 * a));
+        if (x1 < x2){
+            result = "Количество решений: 2. Корни: " + x1 + ";" + x2;
+        } else {
+            result = "Количество решений: 2. Корни: " + x2 + ";" + x1;
         }
 
         return result;
