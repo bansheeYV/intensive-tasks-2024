b7511e3305dd27f57c2d1f2290be0a5bef8d03a1
bansheeYV <bansheew2@gmail.com>
2024-12-17 09:27:25 +0300

    Task4 исправил

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -32,11 +32,11 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        double discriminant = b * b - 4 * a * c;
-
         if (a == 0 && b == 0 && c == 0){
             return "Бесконечное множество решений.";
         }
+
+        double discriminant = b * b - 4 * a * c;
 
         if (discriminant < 0 || a == 0 && b == 0){
             return "Количество решений: 0.";
@@ -54,15 +54,12 @@ public class Task4 {
             return "Количество решений: 1. Корень: " + (-c / b);
         }
 
-        String result = "";
         double x1 = ((-b - Math.sqrt(discriminant)) / (2 * a));
         double x2 = ((-b + Math.sqrt(discriminant)) / (2 * a));
         if (x1 < x2){
-            result = "Количество решений: 2. Корни: " + x1 + ";" + x2;
+            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
         } else {
-            result = "Количество решений: 2. Корни: " + x2 + ";" + x1;
+            return "Количество решений: 2. Корни: " + x2 + ";" + x1;
         }
-
-        return result;
     }
 }
